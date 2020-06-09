7a5edccbf0239ff6231a9fc4e8c4adff22c28ceb
pponikowski <ponikowski.pawel19@gmail.com>
2020-06-09 17:27:07 +0200

    Update

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,7 +1,8 @@
 const request = require('request-promise');
 
+//require('dotenv').config();
+
 const API_KEY = '7ad2bed15cd63282563dacd50b99525f';
-require('dotenv').config();
 
 class Weather {
   static retrieveByCity (city, callback) {
